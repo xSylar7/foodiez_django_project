a18f406a343a4074c7dd042239b5632bfa92fc6d
Khaled ALBuloushi <k.albloushii@hotmail.com>
2022-12-04 20:43:45 +0300

    started working on category and recipies models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+
+
+class Recipies (models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'recipies')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name= 'recipies')
